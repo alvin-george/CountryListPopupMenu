a6d14d063a289e7bab18be8a4f6851c7f0ba1aa1
Alvin George <alvin@sodio.tech>
2017-07-14 12:58:05 +0530

    country list. initial

--- CountryListPicker/ViewController.swift
+++ CountryListPicker/ViewController.swift
@@ -8,13 +8,35 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITextFieldDelegate {
 
+    @IBOutlet weak var countryTextfield: UITextField!
+    @IBOutlet weak var stateTextfield: UITextField!
+    @IBOutlet weak var cityTextfield: UITextField!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        let countries = NSLocale.isoCountryCodes.map { (code:String) -> String in
+            let id = NSLocale.localeIdentifier(fromComponents: [NSLocale.Key.countryCode.rawValue: code])
+            return NSLocale(localeIdentifier: "en_US").displayName(forKey: NSLocale.Key.identifier, value: id) ?? "Country not found for code: \(code)"
+        }
+        
+        print(countries)
+        print(countries.count)
+        
     }
-
+    func textFieldDidBeginEditing(_ textField: UITextField) {
+        
+    }
+    func textFieldDidEndEditing(_ textField: UITextField) {
+        
+    }
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        
+        textField.resignFirstResponder()
+        return true
+    }
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
